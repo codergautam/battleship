0539bb565d492910d5721e80c6903b5a9284982a
Coder Gautam <gautamgxtv@gmail.com>
2021-04-20 07:55:08 -0500

    forgot to return lol

--- src/classes/Ship.php
+++ src/classes/Ship.php
@@ -30,6 +30,7 @@ class Ship {
             }
 
         }
+        return $points;
     }
 }
 ?>
--- src/debug.php
+++ src/debug.php
@@ -1,7 +1,7 @@
 <?php
 include_once("classes/classes.php");
 //$pos = new Position("A2");
-$ship = new Ship(new Position("C1"), 3, true);
+$ship = new Ship(new Position("C1"), 2, false);
 echo "<pre>";
 var_dump($ship->getPoints());
 echo "</pre>";
